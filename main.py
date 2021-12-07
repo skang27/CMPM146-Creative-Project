22f99a8fa3289a4344eb8574bd9580c99277c7b1
skang27 <skang27@ucsc.edu>
2021-12-07 00:14:22 -0800

    made it so that npc_state is now randomized, added print statements for beginning traits

--- main.py
+++ main.py
@@ -56,26 +56,38 @@ def rank_options(options, state):
     return ranked_opts
 
 
+def print_traits(npc_state):
+    print("\nHere are your NPC's traits:")
+    print("Patience:", round(npc_state[0], 2))
+    print("Curiosity:", round(npc_state[1], 2))
+    print("Honesty:", round(npc_state[2], 2))
+    print("Empathy:", round(npc_state[3], 2))
+    print("Decisiveness:", round(npc_state[4], 2))
+    print("Cynicism:", round(npc_state[5], 2))
+    print("")
+
+
 if __name__ == '__main__':
     # Possible traits for the NPC
-    npc_state = [2, 2, 2, 2, 2, 2]
+    npc_state = []
 
-    not_done = True
+    for i in range(6):
+        npc_state.append(random.randint(-5, 5))
+
+    is_done = False
 
     script_filename = 'script.json'
     with open(script_filename) as f:
         data = json.load(f)
 
-    while not_done:
+    while not is_done:
+        print_traits(npc_state)
         for scene_dict in data:
             options_list = scene_dict["Options"]
             if len(options_list) == 0:  # if there are no options, then it is just a scene
                 next_to_print = scene_dict["Scene"]
                 print(next_to_print, "\n")
             else:
-                # print("**options found**\n")
-                # TODO: Use utility functions to weigh each option (should have a dictionary with options).
-                #       Should also print selected option in this if statement.
                 ranked_opts = rank_options(scene_dict["Options"], npc_state)
 
                 for util, opt, index in ranked_opts:
@@ -89,9 +101,6 @@ if __name__ == '__main__':
 
                 print("")
 
-                # TODO: Figure out how we're going to set up options along with their trait values
-                #       Finalize state
-                #       Add player input and the part that changes state according to the player's input
                 favorability = input("How favorable is this action from -5 to 5? ")
 
                 print("")
@@ -99,9 +108,6 @@ if __name__ == '__main__':
                 opt = find_opt(scene_dict["Options"], ranked_opts[0][1])
                 npc_state = change_trait(npc_state, int(favorability), opt)
 
-                # after the player says how favorable the NPC's choice was,
-                # print the rest of the script that follows the option the NPC chose
-                # get the index of the chosen from the original list index stored in the rank option function
                 next_to_print = options_list[ranked_opts[0][2]]["Result"]
 
                 print(next_to_print)
@@ -109,15 +115,7 @@ if __name__ == '__main__':
                 # if len(next_to_print) < 5:
                 #     time.sleep()
 
-                print("")
-
+        print_traits(npc_state)
         finish = input("Would you like to restart the script with the current traits? (Y/N) ")
         if finish == "N" or finish == "n":
-            print("\nHere are your NPC's traits:")
-            print("Patience:", round(npc_state[0],2))
-            print("Curiosity:", round(npc_state[1],2))
-            print("Honesty:", round(npc_state[2],2))
-            print("Empathy:", round(npc_state[3],2))
-            print("Decisiveness:", round(npc_state[4],2))
-            print("Cynicism:", round(npc_state[5],2))
-            not_done = False
+            is_done = True
